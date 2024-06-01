bd17486e6b3d2539a3cea51d8202d73b9a0750e6
Adam Fazli <120200477+AdamFazli@users.noreply.github.com>
2024-06-02 00:48:29 +0800

    Code admin nanti hantar

---
+++ lib/view/berita/add_event_page.dart
@@ -0,0 +1,253 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:mtsp/global.dart';
+
+class AddEventPage extends StatefulWidget {
+  final Function addEvent;
+
+  AddEventPage({required this.addEvent});
+
+  @override
+  _AddEventPageState createState() => _AddEventPageState();
+}
+
+class _AddEventPageState extends State<AddEventPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _title = '';
+  String _description = '';
+  DateTime? _date;
+  TimeOfDay? _time;
+  String _location = '';
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Add Event',
+          style: GoogleFonts.poppins(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: primaryColor,
+        iconTheme: IconThemeData(color: Colors.white), // Change back button color to white
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(4.0),
+          child: Container(
+            color: Colors.black,
+            height: 2,
+          ),
+        ),
+      ),
+      backgroundColor: secondaryColor,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                  labelStyle: TextStyle(color: Colors.white),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
+                style: TextStyle(color: Colors.white),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter event title';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _title = value!;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                  labelStyle: TextStyle(color: Colors.white),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
+                style: TextStyle(color: Colors.white),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter event description';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _description = value!;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Date',
+                  labelStyle: TextStyle(color: Colors.white),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.calendar_today, color: Colors.white),
+                    onPressed: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2101),
+                      );
+                      if (pickedDate != null) {
+                        setState(() {
+                          _date = pickedDate;
+                        });
+                      }
+                    },
+                  ),
+                ),
+                style: TextStyle(color: Colors.white),
+                readOnly: true,
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2101),
+                  );
+                  if (pickedDate != null) {
+                    setState(() {
+                      _date = pickedDate;
+                    });
+                  }
+                },
+                validator: (value) {
+                  if (_date == null) {
+                    return 'Please select a date';
+                  }
+                  return null;
+                },
+                controller: TextEditingController(
+                  text: _date == null ? '' : '${_date!.day}/${_date!.month}/${_date!.year}',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Time',
+                  labelStyle: TextStyle(color: Colors.white),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.access_time, color: Colors.white),
+                    onPressed: () async {
+                      TimeOfDay? pickedTime = await showTimePicker(
+                        context: context,
+                        initialTime: TimeOfDay.now(),
+                      );
+                      if (pickedTime != null) {
+                        setState(() {
+                          _time = pickedTime;
+                        });
+                      }
+                    },
+                  ),
+                ),
+                style: TextStyle(color: Colors.white),
+                readOnly: true,
+                onTap: () async {
+                  TimeOfDay? pickedTime = await showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay.now(),
+                  );
+                  if (pickedTime != null) {
+                    setState(() {
+                      _time = pickedTime;
+                    });
+                  }
+                },
+                validator: (value) {
+                  if (_time == null) {
+                    return 'Please select a time';
+                  }
+                  return null;
+                },
+                controller: TextEditingController(
+                  text: _time == null ? '' : _time!.format(context),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Location',
+                  labelStyle: TextStyle(color: Colors.white),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
+                style: TextStyle(color: Colors.white),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter event location';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _location = value!;
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _pickImage,
+                child: Text('Upload Image'),
+              ),
+              if (_image != null)
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: Image.file(_image!),
+                ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    widget.addEvent({
+                      'title': _title,
+                      'description': _description,
+                      'date': _date,
+                      'time': _time,
+                      'location': _location,
+                      'image': _image,
+                    });
+                    Navigator.pop(context);
+                  }
+                },
+                child: Text('Save Event'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/view/berita/berita.dart
+++ lib/view/berita/berita.dart
@@ -1,9 +1,12 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+//Berita (User)
+
+// ignore_for_file: prefer_const_constructors, use_super_parameters, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mtsp/global.dart';
 import 'package:mtsp/widgets/drawer.dart';
+import 'detail_page.dart';
 
 class Berita extends StatefulWidget {
   const Berita({Key? key}) : super(key: key);
@@ -14,6 +17,34 @@ class Berita extends StatefulWidget {
 
 class _BeritaState extends State<Berita> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void navigateToDetail(String title, String date, String description, String imageUrl) {
+    Navigator.push(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => DetailPage(
+          title: title,
+          date: date,
+          description: description,
+          imageUrl: imageUrl,
+          headerColor: Color(0xff06142F),
+          backgroundColor: Color(0xff06142F),
+        ),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const begin = Offset(1.0, 0.0);
+          const end = Offset.zero;
+          const curve = Curves.ease;
+
+          var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+          return SlideTransition(
+            position: animation.drive(tween),
+            child: child,
+          );
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,127 +101,147 @@ class _BeritaState extends State<Berita> {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
+            GestureDetector(
+              onTap: () {
+                navigateToDetail(
+                  'Ceramah Khas Ustaz Kazim Elias : Kitab Anak Kunci Syurga',
+                  '28 Julai 2019 (Ahad)',
+                  'Ceramah ini akan membahas isi penting dalam Kitab Anak Kunci Syurga, yang pasti memberi manfaat kepada kita semua dalam meningkatkan pemahaman agama dan keimanan.',
+                  'lib/images/Ustaz Kazim Elias.jpg',
+                );
+              },
+              child: Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 200, 
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        image: DecorationImage(
-                          image: AssetImage('lib/images/Ustaz Kazim Elias.jpg'),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 16.0),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Icon(
-                          Icons.access_time,
-                          color: Colors.black,
-                          size: 24,
-                        ),
-                        SizedBox(width: 8.0),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '28 Julai 2019 (Ahad)',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              SizedBox(height: 8.0),
-                              Text(
-                                'Ceramah Khas Ustaz Kazim Elias : Kitab Anak Kunci Syurga',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black,
-                                ),
-                                maxLines: 2, 
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ],
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          image: DecorationImage(
+                            image: AssetImage('lib/images/Ustaz Kazim Elias.jpg'),
+                            fit: BoxFit.cover,
                           ),
                         ),
-                      ],
-                    ),
-                  ],
+                      ),
+                      SizedBox(height: 16.0),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Icon(
+                            Icons.access_time,
+                            color: Colors.black,
+                            size: 24,
+                          ),
+                          SizedBox(width: 8.0),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '28 Julai 2019 (Ahad)',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                SizedBox(height: 8.0),
+                                Text(
+                                  'Ceramah Khas Ustaz Kazim Elias : Kitab Anak Kunci Syurga',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                  ),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
+            GestureDetector(
+              onTap: () {
+                navigateToDetail(
+                  'Ceramah Parenting “Ibu Bapa Gemilang, Anak Cemerlang” oleh Prof Madya DR Mohd Nor Mamat',
+                  '9 Disember 2023 (Ahad)',
+                  'Ceramah ini akan membahas isi penting tentang parenting yang pasti memberi manfaat kepada kita semua dalam meningkatkan pemahaman tentang pendidikan anak.',
+                  'lib/images/Prof Madya Mohd Nor.jpg',
+                );
+              },
+              child: Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 200, 
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        image: DecorationImage(
-                          image: AssetImage('lib/images/Prof Madya Mohd Nor.jpg'),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 16.0),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Icon(
-                          Icons.access_time,
-                          color: Colors.black,
-                          size: 24,
-                        ),
-                        SizedBox(width: 8.0),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '9 Disember 2023 (Ahad)',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              SizedBox(height: 8.0),
-                              Text(
-                                'Ceramah Parenting “Ibu Bapa Gemilang, Anak Cemerlang” oleh Prof Madya DR Mohd Nor Mamat',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black,
-                                ),
-                                maxLines: 2, 
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ],
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          image: DecorationImage(
+                            image: AssetImage('lib/images/Prof Madya Mohd Nor.jpg'),
+                            fit: BoxFit.cover,
                           ),
                         ),
-                      ],
-                    ),
-                  ],
+                      ),
+                      SizedBox(height: 16.0),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Icon(
+                            Icons.access_time,
+                            color: Colors.black,
+                            size: 24,
+                          ),
+                          SizedBox(width: 8.0),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '9 Disember 2023 (Ahad)',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                SizedBox(height: 8.0),
+                                Text(
+                                  'Ceramah Parenting “Ibu Bapa Gemilang, Anak Cemerlang” oleh Prof Madya DR Mohd Nor Mamat',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                  ),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
